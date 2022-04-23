f868b1eb1aa9e3306c3e078d4b56b3038c1a9001
David Martinez Marti <deavidsedice@gmail.com>
2022-04-23 17:19:58 +0100

    refactor into library and add logging and other fun libraries

--- yarrpass/src/main.rs
+++ yarrpass/src/main.rs
@@ -1,78 +1,43 @@
-extern crate base64;
-use orion::aead;
-use orion::kdf;
-use std::time::Instant;
+use anyhow::{Context, Result};
 
-fn flush() {
-    use std::io::Write;
-    std::io::stdout()
-        .flush()
-        .expect("Error flushing STDOUT :-(");
-}
+use yarrpasslib::SaltAndCipher;
+use yarrpasslib::{get_password, get_password_str};
 
-fn get_password(ask: &str) -> Vec<u8> {
-    print!("{}: ", ask);
-    flush();
-    let password = rpassword::read_password().unwrap();
-    password.into_bytes()
-}
-
-fn main() {
-    match std::env::args().nth(1) {
-        Some(c) => decode(c),
-        None => encode(),
+fn main() -> Result<()> {
+    // CHECK THE RUST_LOG ENV VARIABLE
+    if std::env::var("RUST_LOG").is_err() {
+        // IF NOT SET, SET IT TO INFO.
+        std::env::set_var("RUST_LOG", "INFO");
     }
+    env_logger::init();
+    match std::env::args().nth(1) {
+        Some(c) => decode(c).context("Decode step failed")?,
+        None => encode().context("Encode step failed")?,
+    }
+    Ok(())
 }
 
-// TODO: If salt is in the message, it will need a key derivation each step,
-//       and the raw password to be held in memory for longer periods.
-
-fn decode(ciphertext: String) {
+fn decode(ciphertext: String) -> Result<()> {
     println!("**** DECODE ****");
-    let mut cipherv = ciphertext.split('|');
-    let salt64 = cipherv.next().expect("missing salt part from ciphertext");
-    let cipher64 = cipherv
-        .next()
-        .expect("missing cipher part - did you forget <|> ?");
-    let salt = base64::decode_config(salt64, base64::URL_SAFE_NO_PAD).unwrap();
-    let ciphertext = base64::decode_config(cipher64, base64::URL_SAFE_NO_PAD).unwrap();
+    let sc = SaltAndCipher::deserialize(&ciphertext)?;
 
-    let pass = get_password("Input Password");
-    let user_password = kdf::Password::from_slice(&pass).unwrap();
-
-    let salt = kdf::Salt::from_slice(&salt).unwrap();
-
-    let now = Instant::now();
-    let derived_key = kdf::derive_key(&user_password, &salt, 4, 1 << 12, 32).unwrap();
-    let elapsed = now.elapsed();
-    println!("Derivation took {:?}", elapsed);
-
-    let decrypted_data = aead::open(&derived_key, &ciphertext).unwrap();
-    println!("decrypted_data: {:?}", std::str::from_utf8(&decrypted_data));
+    let pass = get_password("Input Password")?;
+    let dec = sc.decrypt(&pass)?;
+    println!("decrypted_data: {:?}", dec);
+    Ok(())
 }
 
-fn encode() {
+fn encode() -> Result<()> {
     println!("**** ENCODE ****");
-    let pass = get_password("Input Password");
-    let secret_message = get_password("Input Secret Message");
+    let pass = get_password("Input Password")?;
+    let secret_message = get_password_str("Input Secret Message")?;
+    let sc = SaltAndCipher::new(&pass, &secret_message)?;
+    let token = sc.serialize();
 
-    let user_password = kdf::Password::from_slice(&pass).unwrap();
-    let salt = kdf::Salt::default();
-
-    let now = Instant::now();
-    let derived_key = kdf::derive_key(&user_password, &salt, 4, 1 << 12, 32).unwrap();
-    let elapsed = now.elapsed();
-    println!("Derivation took {:?}", elapsed);
-
-    let ciphertext = aead::seal(&derived_key, &secret_message).unwrap();
-
-    let decrypted_data = aead::open(&derived_key, &ciphertext).unwrap();
+    let decrypted_data = SaltAndCipher::deserialize(&token)?.decrypt(&pass)?;
     if secret_message != decrypted_data {
         panic!("Secret message and decrypted message do not match. Unknown error!")
     }
-    let salt64 = base64::encode_config(&salt, base64::URL_SAFE_NO_PAD);
-    let cipher64 = base64::encode_config(&ciphertext, base64::URL_SAFE_NO_PAD);
-
-    println!("{}|{}", &salt64, &cipher64);
-    println!("secret: {:?}", std::str::from_utf8(&decrypted_data));
+    println!("Token: {}", &token);
+    Ok(())
 }
---
+++ yarrpasslib/src/lib.rs
@@ -0,0 +1,103 @@
+extern crate base64;
+use anyhow::Context;
+use anyhow::Result;
+use log::debug;
+use orion::aead;
+use orion::kdf;
+use std::time::Instant;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PassError {
+    #[error("salt not found in text {0:?}")]
+    MissingSalt(String),
+    #[error("ciphertext not found in text, did you miss a pipe <|>? {0:?}")]
+    MissingCiphertext(String),
+    #[error("unknown error")]
+    Unknown,
+}
+
+pub fn flush() {
+    use std::io::Write;
+    std::io::stdout()
+        .flush()
+        .expect("Error flushing STDOUT :-(");
+}
+
+pub fn get_password(ask: &str) -> Result<Vec<u8>> {
+    print!("{}: ", ask);
+    flush();
+    let password = rpassword::read_password()?;
+    Ok(password.into_bytes())
+}
+
+pub fn get_password_str(ask: &str) -> Result<String> {
+    print!("{}: ", ask);
+    flush();
+    Ok(rpassword::read_password()?)
+}
+
+pub fn b64_enc(v: &[u8]) -> String {
+    base64::encode_config(v, base64::URL_SAFE_NO_PAD)
+}
+
+pub fn b64_dec(v: &str) -> Result<Vec<u8>> {
+    Ok(base64::decode_config(v, base64::URL_SAFE_NO_PAD)?)
+}
+
+pub struct SaltAndCipher {
+    pub salt: kdf::Salt,
+    pub ciphertext: Vec<u8>,
+}
+
+impl SaltAndCipher {
+    pub fn serialize(&self) -> String {
+        let salt64 = b64_enc(self.salt.as_ref());
+        let cipher64 = b64_enc(&self.ciphertext);
+        format!("{}|{}", salt64, cipher64)
+    }
+    pub fn deserialize(text: &str) -> Result<Self> {
+        let mut cipherv = text.split('|');
+        let salt64 = cipherv
+            .next()
+            .ok_or_else(|| PassError::MissingSalt(text.to_string()))?;
+        let cipher64 = cipherv
+            .next()
+            .ok_or_else(|| PassError::MissingCiphertext(text.to_string()))?;
+
+        let salt = b64_dec(salt64)
+            .with_context(|| format!("deserializing {:?} failed to do base64 decode", text))?;
+        let salt = kdf::Salt::from_slice(&salt)?;
+        let ciphertext = b64_dec(cipher64)?;
+
+        Ok(SaltAndCipher { salt, ciphertext })
+    }
+    pub fn derive_key(&self, pass: &[u8]) -> Result<kdf::SecretKey> {
+        Self::derive_key_salt(pass, &self.salt)
+    }
+    pub fn derive_key_salt(pass: &[u8], salt: &kdf::Salt) -> Result<kdf::SecretKey> {
+        let user_password = kdf::Password::from_slice(pass)?;
+        let now = Instant::now();
+        let derived_key = kdf::derive_key(&user_password, salt, 4, 1 << 12, 32)?;
+        let elapsed = now.elapsed();
+        debug!("Derivation took {:?}", elapsed);
+        Ok(derived_key)
+    }
+    pub fn decrypt(&self, pass: &[u8]) -> Result<String> {
+        let key = self.derive_key(pass)?;
+        let decrypted_data = aead::open(&key, &self.ciphertext)?;
+        Ok(std::str::from_utf8(&decrypted_data)?.to_string())
+    }
+    pub fn new(pass: &[u8], message: &str) -> Result<Self> {
+        let salt = kdf::Salt::default();
+        let key = Self::derive_key_salt(pass, &salt)?;
+        let message = message.as_bytes();
+        let ciphertext = aead::seal(&key, message)?;
+        Ok(Self { salt, ciphertext })
+    }
+}
+
+// TODO: If salt is in the message, it will need a key derivation each step,
+//       and the raw password to be held in memory for longer periods.
+
+mod tests;
