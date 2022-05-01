80ca1f0544aeba86263f9ec2f1a30d867510631c
David Martinez Marti <deavidsedice@gmail.com>
2022-05-02 00:02:27 +0100

    add currently playing

---
+++ whatsongisplaying.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dependency:
+# $ sudo apt install playerctl
+
+while :
+do
+	# playerctl metadata --format "Now playing: {{ artist }} - {{ album }} - {{ title }}               " > .currentlyplaying.txt
+	playerctl metadata --format "Now playing: {{ artist }} - {{ title }}               " > .currentlyplaying.txt
+    # copying prevents flickering in OBS (Bash first creates an empty file)
+    cp .currentlyplaying.txt currentlyplaying.txt
+	sleep 1
+done
