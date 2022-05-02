5cf82e5db1c07501476b2a39acaef4f89e05651d
David Martinez Marti <deavidsedice@gmail.com>
2022-05-02 09:07:39 +0100

    fixes for whatsongisplaying

--- whatsongisplaying.sh
+++ whatsongisplaying.sh
@@ -6,7 +6,8 @@
 while :
 do
 	# playerctl metadata --format "Now playing: {{ artist }} - {{ album }} - {{ title }}               " > .currentlyplaying.txt
-	playerctl metadata --format "Now playing: {{ artist }} - {{ title }}               " > .currentlyplaying.txt
+	# playerctl metadata --format "Now playing: {{ artist }} - {{ title }}               " > .currentlyplaying.txt
+    playerctl metadata --all-players --format '{{ status }}: {{ artist }} - {{ title }}               ' | grep Playing | tr '\n' ' ' > .currentlyplaying.txt
     # copying prevents flickering in OBS (Bash first creates an empty file)
     cp .currentlyplaying.txt currentlyplaying.txt
 	sleep 1
