7ed033d66955f116f4278d0f88ff172e0946739d
David Martinez Marti <deavidsedice@gmail.com>
2022-05-11 18:43:17 +0100

    Split by lines and add drop shadow

---
+++ yarrsvg/src/fontsz.rs
@@ -0,0 +1,160 @@
+use std::path::PathBuf;
+
+use log::{debug, warn};
+
+pub struct Font {
+    face: ttf_parser::Face<'static>,
+    pub size: f64,
+}
+
+impl Font {
+    pub fn new(path: PathBuf) -> Self {
+        let font_data = std::fs::read(path).unwrap();
+        let font_lk: &'static [u8] = Box::leak(font_data.into_boxed_slice());
+        let face = match ttf_parser::Face::from_slice(font_lk, 0) {
+            Ok(f) => f,
+            Err(e) => {
+                eprint!("Error: {}.", e);
+                std::process::exit(1);
+            }
+        };
+        Self { face, size: 1.0 }
+    }
+    pub fn print_info(&self) {
+        let family_name = self
+            .face
+            .names()
+            .into_iter()
+            .find(|name| name.name_id == ttf_parser::name_id::FULL_NAME && name.is_unicode())
+            .and_then(|name| name.to_string());
+
+        let post_script_name = self
+            .face
+            .names()
+            .into_iter()
+            .find(|name| name.name_id == ttf_parser::name_id::POST_SCRIPT_NAME && name.is_unicode())
+            .and_then(|name| name.to_string());
+
+        println!("Family name: {:?}", family_name);
+        println!("PostScript name: {:?}", post_script_name);
+        println!("Units per EM: {:?}", self.face.units_per_em());
+        println!("Ascender: {}", self.face.ascender());
+        println!("Descender: {}", self.face.descender());
+        println!("Line gap: {}", self.face.line_gap());
+        println!("Global bbox: {:?}", self.face.global_bounding_box());
+        println!("Number of glyphs: {}", self.face.number_of_glyphs());
+        println!("Underline: {:?}", self.face.underline_metrics());
+        println!("X height: {:?}", self.face.x_height());
+        println!("Weight: {:?}", self.face.weight());
+        println!("Width: {:?}", self.face.width());
+        println!("Regular: {}", self.face.is_regular());
+        println!("Italic: {}", self.face.is_italic());
+        println!("Bold: {}", self.face.is_bold());
+        println!("Oblique: {}", self.face.is_oblique());
+        println!("Strikeout: {:?}", self.face.strikeout_metrics());
+        println!("Subscript: {:?}", self.face.subscript_metrics());
+        println!("Superscript: {:?}", self.face.superscript_metrics());
+        println!("Variable: {:?}", self.face.is_variable());
+    }
+    pub fn char_width(&self, text: &str) -> Vec<f64> {
+        let mut total_width: Vec<f64> = vec![];
+        let uem = self.face.units_per_em();
+        for ch in text.chars() {
+            if let Some(glyph) = self.face.glyph_index(ch) {
+                if let Some(width) = self.face.glyph_hor_advance(glyph) {
+                    debug!("Char {:?} width: {:?}", ch, width);
+                    total_width.push(width as f64 * self.size / uem as f64);
+                } else {
+                    warn!("Char {:?} doesn't have width", ch);
+                }
+                // FIXME: There's some slight discrepancy between this and SVG rendered
+                //   text. Might be a font mismatch or some setting, or the side bearings.
+                //   All that we tested seems to be worse than nothing.
+                // if let Some(bearing) = self.face.glyph_hor_side_bearing(glyph) {
+                //     debug!("Char {:?} bearing: {:?}", ch, bearing);
+                //     total_width.push(-bearing as f64 / uem as f64 * 0.0);
+                // }
+            } else {
+                warn!("Char {:?} doesn't exist in font", ch);
+            }
+        }
+        total_width
+    }
+    pub fn split_lines(&self, text: &str, maxwidth: f64) -> Vec<String> {
+        let mut ret: Vec<String> = vec![];
+        let uem = self.face.units_per_em();
+        let mut xpos = 0.0;
+        let mut line = String::new();
+
+        for ch in text.chars() {
+            if let Some(glyph) = self.face.glyph_index(ch) {
+                if let Some(width) = self.face.glyph_hor_advance(glyph) {
+                    let width: f64 = width as f64 / uem as f64 * self.size;
+                    if xpos + width < maxwidth {
+                        line.push(ch);
+                        xpos += width;
+                    } else {
+                        ret.push(line.clone());
+                        line.clear();
+                        line.push(ch);
+                        xpos = width;
+                    }
+                }
+            }
+        }
+        ret.push(line);
+
+        ret
+    }
+}
+
+pub struct FontDB {
+    pub db: fontdb::Database,
+}
+
+impl FontDB {
+    pub fn new() -> Self {
+        let now = std::time::Instant::now();
+        let mut db = fontdb::Database::new();
+        db.load_system_fonts();
+        // db.set_serif_family("Times New Roman");
+        // db.set_sans_serif_family("Arial");
+        // db.set_cursive_family("Comic Sans MS");
+        // db.set_fantasy_family("Impact");
+        // db.set_monospace_family("Courier New");
+        debug!(
+            "Loaded {} font faces in {}ms.",
+            db.len(),
+            now.elapsed().as_millis()
+        );
+        FontDB { db }
+    }
+    pub fn query(&self, family_name: &str) -> Option<PathBuf> {
+        let now = std::time::Instant::now();
+        let query = fontdb::Query {
+            families: &[fontdb::Family::Name(family_name), fontdb::Family::SansSerif],
+            weight: fontdb::Weight::BOLD,
+            ..fontdb::Query::default()
+        };
+        match self.db.query(&query) {
+            Some(id) => {
+                let (src, index) = self.db.face_source(id).unwrap();
+                if let fontdb::Source::File(ref path) = &src {
+                    debug!(
+                        "Font '{}':{} found in {}ms.",
+                        path.display(),
+                        index,
+                        now.elapsed().as_micros() as f64 / 1000.0
+                    );
+                    return Some(path.clone());
+                } else {
+                    warn!("no font path found for {:?}", family_name);
+                }
+            }
+            None => {
+                warn!("Error: {:?} not found.", family_name);
+            }
+        }
+        None
+    }
+}
--- yarrsvg/src/main.rs
+++ yarrsvg/src/main.rs
@@ -1,12 +1,18 @@
-use std::path::PathBuf;
+mod fontsz;
 use std::time::Instant;
 
 use anyhow::Result;
-use log::debug;
 use log::error;
+use log::info;
 use log::LevelFilter;
 use svg::node;
+use svg::node::element;
+use svg::node::element::Definitions;
+use svg::node::element::Element;
+use svg::node::element::Filter;
+use svg::node::element::Group;
 use svg::Document;
+use svg::Node;
 use yarrdata::db;
 
 const OUT_FILENAME: &str = "./yarrosco_chat.svg";
@@ -52,35 +58,136 @@ async fn main() -> Result<()> {
     //     .add(node::Text::new("content"));
 
     let now = Instant::now();
+
+    let fdb = fontsz::FontDB::new();
+    let dejavu_path = fdb.query("Noto Sans").expect("error retrieving font");
+    let mut dejavu_face = fontsz::Font::new(dejavu_path);
+    // dejavu_face.print_info();
+    dejavu_face.size = 18.0;
     // Render an SVG
-    let bottom = 700;
-    let msgsz = data.len();
-    let mut document = Document::new().set("viewBox", (0, 0, 250, bottom));
+    let doc_height: f64 = 700.0;
+    let doc_width: f64 = 700.0;
+    let margin: f64 = 20.0;
+    let mut document = Document::new().set(
+        "viewBox",
+        (
+            0.0_f64,
+            0.0_f64,
+            doc_width + margin * 2.0,
+            doc_height + margin * 2.0,
+        ),
+    );
+    let mut myfilter = Filter::new()
+        .set("id", "MyFilter")
+        .set("filterUnits", "userSpaceOnUse")
+        .set("x", 0)
+        .set("y", 0)
+        .set("width", doc_width + margin * 2.0)
+        .set("height", doc_height + margin * 2.0);
+    let mut blur = Element::new("feGaussianBlur");
+    blur.assign("in", "SourceAlpha");
+    blur.assign("stdDeviation", "8");
+    blur.assign("result", "blur");
+
+    let mut merge = Element::new("feMerge");
+    let mut mergenode = Element::new("feMergeNode");
+    mergenode.assign("in", "blur");
+    merge.append(mergenode);
+    let mut mergenode = Element::new("feMergeNode");
+    mergenode.assign("in", "blur");
+    merge.append(mergenode);
+    let mut mergenode = Element::new("feMergeNode");
+    mergenode.assign("in", "blur");
+    merge.append(mergenode);
+    let mut mergenode = Element::new("feMergeNode");
+    mergenode.assign("in", "SourceGraphic");
+    merge.append(mergenode);
+
+    myfilter = myfilter.add(blur).add(merge);
+    let defs = Definitions::new().add(myfilter);
+    document = document.add(defs);
+
+    //     <defs>
+    //     <filter id="MyFilter" filterUnits="userSpaceOnUse" x="0" y="0" width="710" height="700">
+    //       <feGaussianBlur in="SourceAlpha" stdDeviation="8" result="blur"/>
+    //       <feMerge>
+    //         <feMergeNode in="blur"/>
+    //         <feMergeNode in="blur"/>
+    //         <feMergeNode in="blur"/>
+    //         <feMergeNode in="SourceGraphic"/>
+    //       </feMerge>
+    //     </filter>
+    //   </defs>
+    //  <g filter="url(#MyFilter)" >
+
+    let mut main_group = Group::new().set("filter", "url(#MyFilter)");
+
     // document = document.add(path)
     // document = document.add(text);
     let mut count = 0;
-    for (n, e) in data.iter().enumerate().skip(10) {
+    let mut n = 0.0;
+    for e in data.iter().rev() {
         match e {
             yarrdata::Event::Message(m) => {
-                // TODO: This doesn't wrap lines - we need to compute the widrth
+                // TODO: This doesn't wrap lines - we need to compute the width
                 let text = format!("{}: {}", m.username, m.message);
-                let mp = msgsz - n;
-                let y = bottom as f64 - (mp as f64 * 10.0);
-                let text = create_text(5.0, y, &text);
-                document = document.add(text);
+                let y = doc_height as f64 - (n * dejavu_face.size * 1.2) - margin;
+                if y < 0.0 {
+                    break;
+                }
+
+                let lines = dejavu_face.split_lines(&text, doc_width as f64);
+                for text in lines.iter().rev() {
+                    let y = doc_height as f64 - (n * dejavu_face.size * 1.2) - margin;
+                    n += 1.0;
+                    let width: Vec<f64> = dejavu_face.char_width(text);
+                    let sumwidth: f64 = width.iter().sum();
+                    info!("w: {:.2}px, t: {}", sumwidth, &text);
+                    let text = create_text(margin, y, text);
+                    // let mut xpos: f64 = 5.0;
+                    // for w in width.iter().copied() {
+                    //     let data = element::path::Data::new()
+                    //         .move_to((xpos, y + 1.0))
+                    //         .line_by((w, 1.0))
+                    //         .close();
+                    //     let path = node::element::Path::new()
+                    //         .set("fill", "none")
+                    //         .set("stroke", "black")
+                    //         .set("stroke-width", 0.5)
+                    //         .set("d", data);
+                    //     document = document.add(path);
+                    //     xpos += w;
+                    // }
+
+                    // let data = element::path::Data::new()
+                    //     .move_to((5.0 + sumwidth, y))
+                    //     .line_by((0.0, -szpx))
+                    //     .close();
+
+                    // let path = node::element::Path::new()
+                    //     .set("fill", "none")
+                    //     .set("stroke", "black")
+                    //     .set("stroke-width", 1)
+                    //     .set("d", data);
+                    // document = document.add(path);
+                    main_group = main_group.add(text);
+                }
+                n += 0.5;
+
                 count += 1;
             }
         }
     }
-    println!("Craft: {:?} Count: {}", now.elapsed(), count);
+    document = document.add(main_group);
+
+    info!("Craft: {:?} Count: {}", now.elapsed(), count);
     let now = Instant::now();
 
     svg::save(OUT_FILENAME, &document).unwrap();
-    println!("Save: {:?}", now.elapsed());
+    info!("Save: {:?}", now.elapsed());
 
     // let svgstr = document.to_string();
     // test_usvg_bboxes(&svgstr);
-    test_fontdb();
     Ok(())
 }
 
@@ -107,159 +214,9 @@ fn create_text(x: f64, y: f64, text: &str) -> node::element::Text {
     let y = format!("{:.2}", y);
     node::element::Text::new()
     .set("xml_space", "preserve")
-    .set("style", "font-size:6px;font-weight:800; font-family:Sans;fill:#ffffff;stroke:#001338;stroke-width:1;paint-order:markers stroke fill")
+    .set("style", "font-size:18px;font-weight:700; font-family:'Noto Sans';fill:#ffffff;stroke:#001338;stroke-width:6;paint-order:markers stroke fill")
     .set("x", x)
     .set("y", y)
     .set("id", id)
     .add(node::Text::new(text))
-}
-
-#[allow(dead_code)]
-fn test_fontdb() {
-    let mut db = fontdb::Database::new();
-    let now = std::time::Instant::now();
-    db.load_system_fonts();
-    // db.set_serif_family("Times New Roman");
-    // db.set_sans_serif_family("Arial");
-    // db.set_cursive_family("Comic Sans MS");
-    // db.set_fantasy_family("Impact");
-    // db.set_monospace_family("Courier New");
-    println!(
-        "Loaded {} font faces in {}ms.",
-        db.len(),
-        now.elapsed().as_millis()
-    );
-    const FAMILY_NAME: &str = "DejaVu Sans";
-    let query = fontdb::Query {
-        families: &[fontdb::Family::Name(FAMILY_NAME), fontdb::Family::SansSerif],
-        weight: fontdb::Weight::BOLD,
-        ..fontdb::Query::default()
-    };
-
-    let now = std::time::Instant::now();
-    match db.query(&query) {
-        Some(id) => {
-            let (src, index) = db.face_source(id).unwrap();
-            if let fontdb::Source::File(ref path) = &src {
-                println!(
-                    "Font '{}':{} found in {}ms.",
-                    path.display(),
-                    index,
-                    now.elapsed().as_micros() as f64 / 1000.0
-                );
-                test_ttfparser(path);
-            }
-        }
-        None => {
-            println!("Error: '{}' not found.", FAMILY_NAME);
-        }
-    }
-}
-#[allow(dead_code)]
-fn test_ttfparser(path: &PathBuf) {
-    let font_data = std::fs::read(path).unwrap();
-
-    let now = std::time::Instant::now();
-    // TODO: ttf_parser::Face is referencing &font_data, so both must have compatible lifetimes.
-    // ... this will impose a problem later as we will not be able to have the parser loaded permanently.
-    // An option is looping from 0..face.number_of_glyphs(), craft GlyphId.0=i, and retrieve all glyphs.
-    // By doing this we could cache the widhs into a BHashMap
-    // Still there's no API to retrieve all mappings from char -> glyphId, so we will need to
-    // use the source code of glyph_index and reimplement it ourselves.
-    let face = match ttf_parser::Face::from_slice(&font_data, 0) {
-        Ok(f) => f,
-        Err(e) => {
-            eprint!("Error: {}.", e);
-            std::process::exit(1);
-        }
-    };
-
-    let family_name = face
-        .names()
-        .into_iter()
-        .find(|name| name.name_id == ttf_parser::name_id::FULL_NAME && name.is_unicode())
-        .and_then(|name| name.to_string());
-
-    let post_script_name = face
-        .names()
-        .into_iter()
-        .find(|name| name.name_id == ttf_parser::name_id::POST_SCRIPT_NAME && name.is_unicode())
-        .and_then(|name| name.to_string());
-
-    println!("Family name: {:?}", family_name);
-    println!("PostScript name: {:?}", post_script_name);
-    println!("Units per EM: {:?}", face.units_per_em());
-    println!("Ascender: {}", face.ascender());
-    println!("Descender: {}", face.descender());
-    println!("Line gap: {}", face.line_gap());
-    println!("Global bbox: {:?}", face.global_bounding_box());
-    println!("Number of glyphs: {}", face.number_of_glyphs());
-    println!("Underline: {:?}", face.underline_metrics());
-    println!("X height: {:?}", face.x_height());
-    println!("Weight: {:?}", face.weight());
-    println!("Width: {:?}", face.width());
-    println!("Regular: {}", face.is_regular());
-    println!("Italic: {}", face.is_italic());
-    println!("Bold: {}", face.is_bold());
-    println!("Oblique: {}", face.is_oblique());
-    println!("Strikeout: {:?}", face.strikeout_metrics());
-    println!("Subscript: {:?}", face.subscript_metrics());
-    println!("Superscript: {:?}", face.superscript_metrics());
-    println!("Variable: {:?}", face.is_variable());
-    // TODO: Use this to calculate what fits in a line.
-    // There is still the problem on how to translate these measures into a SVG
-    // with a font-face of 6px.
-    // Most probably: width / face.units_per_em() * 6(px)
-    let test_string = "We are here!";
-    for ch in test_string.chars() {
-        let glyph = face.glyph_index(ch).unwrap();
-        let width = face.glyph_hor_advance(glyph);
-        println!("Char {:?} width: {:?}", ch, width);
-    }
-
-    println!("Elapsed: {}us", now.elapsed().as_micros());
-}
-#[allow(dead_code)]
-fn test_usvg_bboxes(svgstr: &str) {
-    use usvg::NodeExt;
-    let now = Instant::now();
-    let mut opt = usvg::Options::default();
-    opt.fontdb.load_system_fonts();
-
-    println!("LoadFonts: {:?}", now.elapsed());
-    let now = Instant::now();
-    // FIXME: This is not a good idea; this takes a long time to convert the SVG.
-    // usvg/src/text/mod.rs:: text_to_paths seems to contain the code to draw them, but can't understand it yet.
-    let rtree = usvg::Tree::from_str(svgstr, &opt.to_ref()).unwrap();
-    println!("Parsing: {:?}", now.elapsed());
-    let now = Instant::now();
-    for node in rtree.root().descendants() {
-        if !rtree.is_in_defs(&node) {
-            if let Some(bbox) = node.calculate_bbox().and_then(|r| r.to_rect()) {
-                let b = format!(
-                    "{:>5.1}, {:>5.1}, {:>5.1}, {:>5.1}",
-                    bbox.left(),
-                    bbox.right(),
-                    bbox.top(),
-                    bbox.bottom()
-                );
-                debug!("{:?} ?> {}", node.id(), b);
-            }
-
-            // Text bboxes are different from path bboxes.
-            if let usvg::NodeKind::Path(ref path) = *node.borrow() {
-                if let Some(ref bbox) = path.text_bbox {
-                    let b = format!(
-                        "{:>5.1}, {:>5.1}, {:>5.1}, {:>5.1}",
-                        bbox.left(),
-                        bbox.right(),
-                        bbox.top(),
-                        bbox.bottom()
-                    );
-                    debug!("{:?} #> {}", node.id(), b);
-                }
-            }
-        }
-    }
-    println!("Bbox: {:?}", now.elapsed());
 }
